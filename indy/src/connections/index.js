fab5184167e7af42cf9a3b8ef9942a0a768c3d2e
JonAnder33 <71341659+JonAnder33@users.noreply.github.com>
2021-07-02 21:09:15 +0200

    Update index.js

--- indy/src/connections/index.js
+++ indy/src/connections/index.js
@@ -8,6 +8,9 @@ urn:jwm:sovrin.org/connections/v1/offer
 
  */
 
+/**
+ * Establece los tipos de mensajes
+ */
 const MESSAGE_TYPES = {
     OFFER : "urn:sovrin:agent:message_type:sovrin.org/connection_offer",
     REQUEST : "urn:sovrin:agent:message_type:sovrin.org/connection_request",
@@ -18,13 +21,26 @@ exports.MESSAGE_TYPES = MESSAGE_TYPES;
 
 exports.handlers = require('./handlers');
 
+/**
+ * Esta funcion es preparar un pedido y enviarselo al Did destino
+ * @param {*} theirEndpointDid 
+ * @param {*} connection_alias 
+ * @returns 
+ */
 exports.prepareRequest = async function (theirEndpointDid, connection_alias) {
+    // FALTA COMENTAR
     let [myNewDid, myNewVerkey] = await sdk.createAndStoreMyDid(await indy.wallet.get(), {});
+    
+    
     await indy.pool.sendNym(await indy.pool.get(), await indy.wallet.get(), await indy.did.getEndpointDid(), myNewDid, myNewVerkey);
 
     let nonce = uuid();
+
+
     indy.store.pendingRelationships.write(myNewDid, theirEndpointDid, nonce, connection_alias);
 
+    //Se enviara un mensaje a la DID asignada al principio del codigo.
+    //Este mensaje sera de tipo PEDIDO, Y no tendra texto
     return {
         type: MESSAGE_TYPES.REQUEST,
         message: {
@@ -35,6 +51,13 @@ exports.prepareRequest = async function (theirEndpointDid, connection_alias) {
     }
 };
 
+/**
+ * Funcion
+ * @param {*} theirEndpointDid 
+ * @param {*} theirDid 
+ * @param {*} requestNonce 
+ * @returns 
+ */
 exports.acceptRequest = async function (theirEndpointDid, theirDid, requestNonce) {
     let [myDid, myVerkey] = await sdk.createAndStoreMyDid(await indy.wallet.get(), {});
 
@@ -44,6 +67,7 @@ exports.acceptRequest = async function (theirEndpointDid, theirDid, requestNonce
     //     did: theirDid,
     //     verkey: theirVerkey
     // });
+
 
     let meta = JSON.stringify({
         theirEndpointDid: theirEndpointDid,
@@ -59,25 +83,41 @@ exports.acceptRequest = async function (theirEndpointDid, theirDid, requestNonce
         verkey: myVerkey,
         nonce: requestNonce
     };
+
+    //Crea el mensaje de respuesta
     let message = {
         aud: theirDid,
         type: MESSAGE_TYPES.RESPONSE,
         message: await indy.crypto.anonCrypt(theirDid, JSON.stringify(connectionResponse))
     };
+    //Envia el mensaje de respuesta al DID de destino
     return indy.crypto.sendAnonCryptedMessage(theirEndpointDid, message);
 };
 
+
+/**
+ * Funcion que aceptara la solicitud de respuesta
+ * @param {*} myDid 
+ * @param {*} rawMessage 
+ */
 exports.acceptResponse = async function (myDid, rawMessage) {
+    //Comprobara todas las relaciones pendientes y las almacenara en pendingRelationships
     let pendingRelationships = indy.store.pendingRelationships.getAll();
+    //Se crea la variable relationships
     let relationship;
+    //Se recorre cada una de las relaciones de pendingRelationships
     for (let entry of pendingRelationships) {
+        //Si la DID de la relacion coincide con la que hemos introducido en la funcion entonces cojeremos la variable de relationship y la combertiremos en esa relacion
         if (entry.myNewDid === myDid) {
             relationship = entry;
         }
     }
+    //Si la relacion es nula (o lo que es lo mismo, si ninguna DID de pendingRelationships coincidia con la DID inicial) sacara un mensaje de error
     if (!relationship) {
         throw Error("RelationshipNotFound");
-    } else {
+    } 
+    //En caso de que si se haya encontrado la una relacion con la DID entonces...
+    else {
         // base64 decode
         let base64DecodedMessage = Buffer.from(rawMessage, 'base64');
         // anon decrypt
@@ -85,10 +125,12 @@ exports.acceptResponse = async function (myDid, rawMessage) {
         // retrieve theirEndpointDid, theirDid, connection_request_nonce
         let theirDid = message.did;
         let theirVerKey = message.verkey;
-
+        //Si el nonce de la relacion y del mensaje no coinciden se enviara un mensaje de error informativo
         if (relationship.nonce !== message.nonce) {
             throw Error("NoncesDontMatch");
+        //En caso de que si se encuentre
         } else {
+            
             await sdk.storeTheirDid(await indy.wallet.get(), {
                 did: theirDid,
                 verkey: theirVerKey
@@ -99,20 +141,23 @@ exports.acceptResponse = async function (myDid, rawMessage) {
                 alias: relationship.alias,
                 theirEndpointDid: relationship.theirEndpointDid
             });
-            await sdk.createPairwise(await indy.wallet.get(), theirDid, relationship.myNewDid, meta);
 
+            await sdk.createPairwise(await indy.wallet.get(), theirDid, relationship.myNewDid, meta);
             // send acknowledge
             await exports.sendAcknowledgement(relationship.myNewDid, theirDid, relationship.theirEndpointDid);
+            //Se borrara la relacion seleccionada de las relaciones en espera
             indy.store.pendingRelationships.delete(relationship.id);
         }
     }
 };
 
+//This function sends an acknowlegement
 exports.sendAcknowledgement = async function (myDid, theirDid, theirEndpointDid) {
     await indy.crypto.sendAnonCryptedMessage(theirEndpointDid, await indy.crypto.buildAuthcryptedMessage(myDid, theirDid, MESSAGE_TYPES.ACKNOWLEDGE, "Success"));
     // await indy.proofs.sendRequest(myDid, theirDid, 'General-Identity');
 };
 
+//This function accept an acknowlegement
 exports.acceptAcknowledgement = async function (theirDid, encryptedMessage) {
     let myDid = await indy.pairwise.getMyDid(theirDid);
 
