9a650b669556d6d61d748c4eb1bdb4f8a1e61333
JonAnder33 <71341659+JonAnder33@users.noreply.github.com>
2021-10-25 20:12:53 +0200

    Update index.js

--- indy/src/issuer/index.js
+++ indy/src/issuer/index.js
@@ -6,9 +6,6 @@ const blockchain = require('../blockchain/index.js');
 const { obtenerCompra } = require('../blockchain/index.js');
 var i=1;
 
-//Issuer se encarga de crear el esquema(schema), crear la definición del credencial y de enviar la oferta.
-
-//Crea el esquema
 
 exports.createSchema = async function (name, version, attributes) {
 
@@ -18,77 +15,16 @@ exports.createSchema = async function (name, version, attributes) {
         bloque =  indy.blockchain.creaBlock(1,Date.now,attributes,"816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
         indy.blockchain.addBlock(BlockChain,bloque);
     } catch (error) {
-        
+
     }
-    
-  
+
+
     let [id, schema] = await sdk.issuerCreateSchema(await indy.did.getEndpointDid(), name, version, [attributes]);
+    let [id, schema] = await sdk.issuerCreateSchema(await indy.did.getEndpointDid(), name, version, attributes);
     let schemaRequest = await sdk.buildSchemaRequest(await indy.did.getEndpointDid(), schema);
     await sdk.signAndSubmitRequest(await indy.pool.get(), await indy.wallet.get(), await indy.did.getEndpointDid(), schemaRequest);
     await indy.did.pushEndpointDidAttribute('schemas', id);
-};
-
-//Devuelve los esquemas
-
-exports.getSchemas = async function () {
-    let metadata = JSON.parse(await sdk.getDidMetadata(await indy.wallet.get(), await indy.did.getEndpointDid()));
-    let schemas = [];
-    for (let schemaId of metadata.schemas) {
-        let schema = await indy.issuer.getSchema(schemaId);
-        schemas.push(schema);
-    }
-    return schemas;
-};
-
-//Crea la definición del credencial
-
-exports.createCredDef = async function (schemaId, tag) {
-    let schema = await exports.getSchema(schemaId);
-    let [credDefId, credDefJson] = await sdk.issuerCreateAndStoreCredentialDef(await indy.wallet.get(), await indy.did.getEndpointDid(), schema, tag, 'CL', '{"support_revocation": false}');
-    let credDefRequest = await sdk.buildCredDefRequest(await indy.did.getEndpointDid(), credDefJson);
-    await sdk.signAndSubmitRequest(await indy.pool.get(), await indy.wallet.get(), await indy.did.getEndpointDid(), credDefRequest);
-    credDefJson.schemaId_long = schemaId;
-    await indy.did.pushEndpointDidAttribute('credential_definitions', credDefJson);
-};
-
-//Envia el esquema
-
-exports.sendSchema = async function(poolHandle, walletHandle, Did, schema) {
-    let schemaRequest = await sdk.buildSchemaRequest(Did, schema);
-    await sdk.signAndSubmitRequest(poolHandle, walletHandle, Did, schemaRequest)
-};
-
-//Envia la definición del credencial
-
-exports.sendCredDef = async function (poolHandle, walletHandle, did, credDef) {
-    let credDefRequest = await sdk.buildCredDefRequest(did, credDef);
-    await sdk.signAndSubmitRequest(poolHandle, walletHandle, did, credDefRequest);
-};
-
-//Devuelve el esquema
-
-exports.getSchema = async function(schemaId) {
-    let getSchemaRequest = await sdk.buildGetSchemaRequest(await indy.did.getEndpointDid(), schemaId);
-    let getSchemaResponse = await sdk.submitRequest(await indy.pool.get(), getSchemaRequest);
-    let [, schema] = await sdk.parseGetSchemaResponse(getSchemaResponse);
-    return schema;
-};
-
-//Devuelve la definición del credencial(se identifica medianta el id)
-
-exports.getCredDef = async function(poolHandle, did, credDefId) {
-    let getCredDefRequest = await sdk.buildGetCredDefRequest(did, credDefId);
-    let getCredDefResponse = await sdk.submitRequest(poolHandle, getCredDefRequest);
-    return await sdk.parseGetCredDefResponse(getCredDefResponse);
-};
-
-//Devuelve la definición del credencial(se identidica mediante el tag)
-
-exports.getCredDefByTag = async function(credDefTag) {
-    let credDefs = await indy.did.getEndpointDidAttribute('credential_definitions');
-    for(let credDef of credDefs) {
-        if(credDef.tag === credDefTag) {
-            return credDef;
+@@ -75,86 +59,3 @@ exports.getCredDefByTag = async function(credDefTag) {
         }
     }
 };
