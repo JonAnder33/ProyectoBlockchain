31f7c95ddf664152b41f937e97c024e1b7717306
JonAnder33 <71341659+JonAnder33@users.noreply.github.com>
2021-10-25 20:23:51 +0200

    Update index.js

--- ui/chat/index.js
+++ ui/chat/index.js
@@ -2,13 +2,19 @@
 const indy = require('../../indy');
 const TWO_WEEKS_MS = 12096e5;
 
+//Devuelve los mensajes.
+
 exports.getMessages = async function(theirDid) {
     return await indy.pairwise.getAttr(theirDid, 'chatMessages');
 };
 
+//Almacena los mensajes.
+
 exports.storeMessage = async function(theirDid, message) {
     return await indy.pairwise.pushAttribute(theirDid, 'chatMessages', JSON.stringify(message));
 };
+
+//Devuelve los mensajes recibidos hace menos de 2 semanas.
 
 exports.getRecentMessages = async function(theirDid) {
     let messages = await exports.getMessages(theirDid);
