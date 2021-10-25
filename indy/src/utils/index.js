ee69c7a2e3a5c51a61a4f716d375d35a15fcb0b2
JonAnder33 <71341659+JonAnder33@users.noreply.github.com>
2021-10-25 20:22:28 +0200

    Update index.js

--- indy/src/utils/index.js
+++ indy/src/utils/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
+//Esta función paraliza el programa durante los milisegundos indicados.
+
 exports.sleep = function (ms) {
     return new Promise(function (resolve) {
         setTimeout(resolve, ms)
     })
 };
+
+
 
 exports.pathToIndyClientHome = function () {
     return require('os').homedir() + "/.indy_client"
